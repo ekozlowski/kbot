eac14b4d61f9c34fca161984c99c51f6f1f5b653
Ed Kozlowski <Ed.Kozlowski@coxautoinc.com>
2017-06-06 19:23:48 -0500

    Updated version handler

--- handlers/version.py
+++ handlers/version.py
@@ -1,9 +1,9 @@
 help_text = "`version` - I'll tell you what version of my software I'm running."
 
-def handle(command):
+def handle(command, callback):
     """
     We don't care what we were passed.  We just return a dumb version string - (for now)
     :param command:
     :return:
     """
-    return "I'm running version 0.0.1"
+    callback("I'm running version 0.0.1")
