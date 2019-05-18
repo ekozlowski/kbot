4d3a05d87df35c0edee00fc79b0bbd9ec81bf54e
Ed Kozlowski <Ed.Kozlowski@coxautoinc.com>
2019-05-17 23:43:37 -0500

    Reformats handlers using black.

--- handlers/grocery.py
+++ handlers/grocery.py
@@ -2,33 +2,36 @@ help_text = "`grocery <command>` - Use `grocery help` for more info."
 
 grocery_list = []
 
+
 def handle(command, callback):
     global grocery_list
     pieces = command.split()
-    response = '\n'.join([
-        "I know that was something to do with groceries, but I didn't understand what you meant.  Sorry.  :disappointed:",
-        "",
-        "Here are some things you can tell me:",
-        "`grocery add <item>`  adds <item> to your grocery list.",
-        "`grocery remove <item>` removes <item> from your grocery list.",
-        "`grocery list` lists your groceries.",
-        "`grocery clear` clears your list.",
-    ])
-    if pieces[0].lower() == 'help':
+    response = "\n".join(
+        [
+            "I know that was something to do with groceries, but I didn't understand what you meant.  Sorry.  :disappointed:",
+            "",
+            "Here are some things you can tell me:",
+            "`grocery add <item>`  adds <item> to your grocery list.",
+            "`grocery remove <item>` removes <item> from your grocery list.",
+            "`grocery list` lists your groceries.",
+            "`grocery clear` clears your list.",
+        ]
+    )
+    if pieces[0].lower() == "help":
         callback(response)
     try:
         action = pieces[1]
     except IndexError:
         callback(response)
     if pieces[2:]:
-        o = ' '.join(pieces[2:])
-    if action == 'add':
+        o = " ".join(pieces[2:])
+    if action == "add":
         if not o:
             response = "Ok, I need something to add."
         else:
             grocery_list.append(o)
             response = "Got it.  Added {} to your grocery list.".format(o)
-    elif action == 'remove':
+    elif action == "remove":
         if not o:
             response = "Ok, I need something to remove."
         else:
@@ -36,13 +39,12 @@ def handle(command, callback):
                 grocery_list.remove(o)
                 response = "Removed {} from your grocery list.".format(o)
             except:
-                response = "Problem removing grocery {} from list.  Was it there to begin with?".format(o)
-    elif action == 'list':
-        response = '\n'.join([
-            "Here are your groceries:",
-            *grocery_list,
-        ])
-    elif action == 'clear':
+                response = "Problem removing grocery {} from list.  Was it there to begin with?".format(
+                    o
+                )
+    elif action == "list":
+        response = "\n".join(["Here are your groceries:", *grocery_list])
+    elif action == "clear":
         grocery_list = []
-        response = 'Grocery list cleared.  Hope you got what you needed...'
+        response = "Grocery list cleared.  Hope you got what you needed..."
     callback(response)
--- handlers/version.py
+++ handlers/version.py
@@ -1,5 +1,6 @@
 help_text = "`version` - I'll tell you what version of my software I'm running."
 
+
 def handle(command, callback):
     """
     We don't care what we were passed.  We just return a dumb version string - (for now)
